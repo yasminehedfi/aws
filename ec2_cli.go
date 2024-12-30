ef0126946a57386ab2ca7b8493f6f8f45c6b65ec
yasmine hedfi <yasminehedfi@yasmines-MacBook-Air.local>
2024-12-31 00:07:18 +0100

    Initial commit

---
+++ ec2_cli.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/spf13/cobra"
+)
+
+var region string
+
+func createEC2Session() *ec2.EC2 {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(region),
+	})
+	if err != nil {
+		log.Fatalf("Unable to create AWS session: %v", err)
+	}
+	return ec2.New(sess)
+}
+
+func listInstances() {
+	svc := createEC2Session()
+
+	result, err := svc.DescribeInstances(nil)
+	if err != nil {
+		log.Fatalf("Unable to describe instances: %v", err)
+	}
+
+	fmt.Println("Instances:")
+	for _, reservation := range result.Reservations {
+		for _, instance := range reservation.Instances {
+			fmt.Printf("ID: %s, State: %s, Type: %s\n", *instance.InstanceId, *instance.State.Name, *instance.InstanceType)
+		}
+	}
+}
+
+func startInstance(instanceID string) {
+	svc := createEC2Session()
+
+	_, err := svc.StartInstances(&ec2.StartInstancesInput{
+		InstanceIds: []*string{aws.String(instanceID)},
+	})
+	if err != nil {
+		log.Fatalf("Unable to start instance: %v", err)
+	}
+
+	fmt.Printf("Instance %s is starting...\n", instanceID)
+}
+
+func stopInstance(instanceID string) {
+	svc := createEC2Session()
+
+	_, err := svc.StopInstances(&ec2.StopInstancesInput{
+		InstanceIds: []*string{aws.String(instanceID)},
+	})
+	if err != nil {
+		log.Fatalf("Unable to stop instance: %v", err)
+	}
+
+	fmt.Printf("Instance %s is stopping...\n", instanceID)
+}
+func createInstance(imageID, instanceType, keyName string) {
+    svc := createEC2Session()
+
+    runInput := &ec2.RunInstancesInput{
+        ImageId:      aws.String(imageID),       
+        InstanceType: aws.String(instanceType), 
+        KeyName:      aws.String(keyName),      
+        MinCount:     aws.Int64(1),             
+        MaxCount:     aws.Int64(1),             
+    }
+
+    result, err := svc.RunInstances(runInput)
+    if err != nil {
+        log.Fatalf("Erreur lors de la création de l'instance : %v", err)
+    }
+
+    for _, instance := range result.Instances {
+        fmt.Printf("Instance créée avec succès ! ID: %s, Type: %s, AMI: %s\n",
+            *instance.InstanceId, *instance.InstanceType, *instance.ImageId)
+    }
+}
+
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all EC2 instances",
+	Run: func(cmd *cobra.Command, args []string) {
+		listInstances()
+	},
+}
+
+var startCmd = &cobra.Command{
+	Use:   "start [instanceID]",
+	Short: "Start EC2 instance",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		startInstance(args[0])
+	},
+}
+
+var stopCmd = &cobra.Command{
+	Use:   "stop [instanceID]",
+	Short: "Stop EC2 instance",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		stopInstance(args[0])
+	},
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVarP(&region, "region", "r", "us-east-1", "AWS region")
+	rootCmd.AddCommand(listCmd)
+	rootCmd.AddCommand(startCmd)
+	rootCmd.AddCommand(stopCmd)
+	rootCmd.AddCommand(createCmd)
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "ec2-cli",
+	Short: "A CLI tool to interact with AWS EC2",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("EC2 CLI: Use `list`, `start`, or `stop` commands")
+	},
+
+}
+
+var createCmd = &cobra.Command{
+    Use:   "create [imageID] [instanceType] [keyName]",
+    Short: "Créer une nouvelle instance EC2",
+    Args:  cobra.ExactArgs(3), 
+    Run: func(cmd *cobra.Command, args []string) {
+        imageID := args[0]
+        instanceType := args[1]
+        keyName := args[2]
+        createInstance(imageID, instanceType, keyName)
+    },
+}
+
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,21 @@
+module GO
+
+go 1.23.4
+
+require (
+	github.com/aws/aws-sdk-go v1.55.5
+	github.com/spf13/cobra v1.8.1
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.32.7 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.26 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.26 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ec2 v1.198.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.12.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.12.7 // indirect
+	github.com/aws/smithy-go v1.22.1 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+)
